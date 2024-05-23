12129ca7022486467be5a6c28d859716c06b6c0e
PoBukHaBcee <mautfat@mail.ru>
2024-05-23 19:09:41 +0300

    yes

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const euro = 0.9223
+let us_dol = prompt("Введите доллары")
+
+console.log(us_dol * euro, " в евро");
--- js/script.js
+++
@@ -1,5 +0,0 @@
-let result = 0
-
-
-
-document.getElementById("result").innerHTML = result
